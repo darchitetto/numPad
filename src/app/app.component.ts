3aa59650415dbab4db60bbe2c44117337ce72393
Denise Architetto <denise@digital-pioneers.com>
2017-07-04 08:48:31 -0400

    remove unused property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,23 +2,22 @@ import { Component } from '@angular/core';
 import { trigger, state, animate, transition, style } from '@angular/animations';
 
 export class numPad {
-  id: number;
-  value: string;
+  value:string;
 }
 
-const NUMPAD: numPad[] = [
-  { id: 1, value: '1' },
-  { id: 2, value: '2' },
-  { id: 3, value: '3' },
-  { id: 4, value: '4' },
-  { id: 5, value: '5' },
-  { id: 6, value: '6' },
-  { id: 7, value: '7' },
-  { id: 8, value: '8' },
-  { id: 9, value: '9' },
-  { id: 10, value: '.'},
-  { id: 11, value: '0' },
-  { id: 12, value: '<'}
+const NUMPAD:numPad[] = [
+  {value: '1'},
+  {value: '2'},
+  {value: '3'},
+  {value: '4'},
+  {value: '5'},
+  {value: '6'},
+  {value: '7'},
+  {value: '8'},
+  {value: '9'},
+  {value: '.'},
+  {value: '0'},
+  {value: '<'}
 ];
 
 @Component({
